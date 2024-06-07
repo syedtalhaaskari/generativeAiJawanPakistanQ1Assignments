ad9f20729d4a02abcd5a1f8cae77918f0f39969d
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-06-07 22:10:04 +0500

    Delete unnecessary files

--- problem6.py
+++
@@ -1,8 +0,0 @@
-# Write a program to check whether a number is divisible by 7
-
-mark = int(input("Enter a number: "))
-
-if mark % 7 == 0:
-    print(f"{mark} is divisible by 7")
-else:
-    print(f"{mark} is not divisible by 7")
