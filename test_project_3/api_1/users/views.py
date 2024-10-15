bc480a2f3e1d0329245dffc0b3eb22560fc61cc8
syedtalhaaskari97 <syedtalhaaskari@gmail.com>
2024-10-15 19:01:13 +0500

    Complete test_project_3

---
+++ test_project_3/api_1/users/views.py
@@ -0,0 +1,14 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(http_method_names=['GET'])
+def get_data(request):
+    data = [
+        {
+            "name": "Syed Talha Askari",
+        },
+        {
+            "name": "API 1",
+        }
+    ]
+    return Response(data)
