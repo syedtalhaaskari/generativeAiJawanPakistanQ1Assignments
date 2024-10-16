3689a9e7568f26bba51bbfbb0bf422cc8d6cb0bb
syedtalhaaskari97 <syedtalhaaskari@gmail.com>
2024-10-16 15:07:32 +0500

    Update model and do migrations

--- library_management_system/core/models.py
+++ library_management_system/core/models.py
@@ -5,11 +5,20 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 class Genre(models.Model):
     name = models.CharField(max_length=50)
 
-class Books(models.Model):
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    genre = models.ManyToManyField(Genre)
+    author = models.ManyToManyField(Author, related_name="authors")
+    genre = models.ManyToManyField(Genre, related_name="genres")
     published_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
