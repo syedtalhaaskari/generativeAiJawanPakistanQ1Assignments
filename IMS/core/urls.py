f37daf20fea704f443e0561eccd24626222b0847
syedtalhaaskari97 <syedtalhaaskari@gmail.com>
2024-10-22 12:06:29 +0500

    Integrate views with core app

---
+++ IMS/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views.category import get_or_create_category, get_or_update_or_delete_category
+from .views.supplier import get_or_create_supplier, get_or_update_or_delete_supplier
+from .views.product import get_or_create_product
+
+urlpatterns = [
+    path('categories/', get_or_create_category),
+    path('categories/<int:id>/', get_or_update_or_delete_category),
+    path('suppliers/', get_or_create_supplier),
+    path('suppliers/<int:id>/', get_or_update_or_delete_supplier),
+    path('products/', get_or_create_product),
+]
