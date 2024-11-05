06fe5a4957c38c51f49f99685a828f634a5ddeb0
syedtalhaaskari97 <syedtalhaaskari@gmail.com>
2024-11-05 12:58:59 +0500

    Add Product Supplier Model

--- IMS/core/models/product.py
+++ IMS/core/models/product.py
@@ -9,9 +9,22 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=9, decimal_places=4)
     quantity = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product_category")
-    supplier = models.ManyToManyField(Supplier, related_name="product_supplier")
+    supplier = models.ManyToManyField(Supplier, related_name="product_supplier", through='ProductSupplier')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.id} - {self.name}"
+
+class ProductSupplier(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('product', 'supplier')
+
+    def __str__(self):
+        return f"{self.product.name} - {self.supplier.name}"
